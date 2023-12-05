93b2169a0052bfeb91085db31f3cade175e413d5
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-12-05 21:46:28 +0100

    models

---
+++ NoSql-WebApi/Models/Domain/ItemModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NoSql_WebApi.Models.Domain
+{
+    public class ItemModel
+    {
+        int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+    }
+}
