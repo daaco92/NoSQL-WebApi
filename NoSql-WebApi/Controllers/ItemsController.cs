e07aafa6f3029ce80a869db51275739829f3f53d
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-12-05 22:27:20 +0100

    ItemsController

---
+++ NoSql-WebApi/Controllers/ItemsController.cs
@@ -0,0 +1,25 @@
+﻿using NoSql_WebApi.Models.Domain;
+using Amazon.DynamoDBv2.DataModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NoSql_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IDynamoDBContext _context;
+        
+        public ItemsController(IDynamoDBContext context)
+        {
+                _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var items = await _context.ScanAsync<ItemModel>(default).GetRemainingAsync();
+            return Ok(items);
+        }
+    }
+}
